01cc8601c1f120041cce957bf0dfae260edf5c6b
Gary Lloyd <gareth.lloyd@tessella.com>
2018-04-28 21:54:57 +0100

    Update provider to use authentication

--- tf-test/provider.tf
+++ tf-test/provider.tf
@@ -3,6 +3,8 @@ terraform {
     address = "http://localhost:8000/state/datalabs"
     lock_address = "http://localhost:8000/state/datalabs"
     unlock_address = "http://localhost:8000/state/datalabs"
+    username = "username"
+    password = "password"
   }
 }
 
